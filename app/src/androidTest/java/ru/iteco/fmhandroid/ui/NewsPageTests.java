038acd66e043db386e2f42148c280aa190d7c1ca
Elizaveta <evshkryabiy@gmail.com>
2023-05-28 18:49:52 +0300

    allure reports added

--- app/src/androidTest/java/ru/iteco/fmhandroid/ui/NewsPageTests.java
+++ app/src/androidTest/java/ru/iteco/fmhandroid/ui/NewsPageTests.java
@@ -17,6 +17,7 @@ import static org.hamcrest.core.IsNot.not;
 
 import androidx.test.ext.junit.rules.ActivityScenarioRule;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
+
 import androidx.test.filters.LargeTest;
 
 import org.junit.Rule;
